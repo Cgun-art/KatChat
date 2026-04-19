c2e918fb803bd1891ada45326e81defc1c348f3b
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-04-19 14:28:41 -0300

    Create gif.ts

---
+++ src/webpage/gif.ts
@@ -0,0 +1,27 @@
+import {GIF} | {Tex_HLSL} | {Tex_GLSL} | {rbx}
+
+     MIN_GIF_RES = (32x32)
+     MAX_GIF_RES = (1024x1024)
+     MIN_GIF_FPS = 1
+     MAX_GIF_FPS = 480
+
+if format.img = gif; Created;
+  then export {rbx}
+img.Codec = mp4 img.Gif = gif
+
+    def MaxColors = 256
+    def MaxColors{FPS_MAX} = 255
+
+     print_frames = all
+compress = false
+
+    parseData = to RBXL; create GifBinary(Base2(0, 1)); col = true
+
+return IMG [repeatTimes = 100000000000000000]
+
+    BypassIntengers = true {NoReplace}
+
+
+0xFFF = gif
+
+return; 0
